70672f061c8e7b00ff7530e8bb599938e6287ec4
ShavarshMartirosyan <shavarshmartirosyan.bangits@gmail.com>
2021-10-08 13:26:32 +0400

    created psswordInput molecul and added it into sing in page

---
+++ src/components/pages/sign-in/SignIn.stories.tsx
@@ -0,0 +1,26 @@
+import { text, withKnobs } from '@storybook/addon-knobs';
+import React from 'react';
+import SignIn from './SignIn';
+
+export default {
+  component: SignIn,
+  decorators: [withKnobs],
+  title: 'components/pages/Sign In'
+};
+
+export const Default = () => {
+  return (
+    <SignIn
+      title={text('title', 'Sign In')}
+      subtitle={text('subtitle', 'Login to manage your account')}
+      buttonText={text('buttonText', 'Login')}
+      renderInputs={(InputComponent, name) => {
+        return (
+          <React.Fragment>
+            <InputComponent />
+          </React.Fragment>
+        );
+      }}
+    />
+  );
+};
