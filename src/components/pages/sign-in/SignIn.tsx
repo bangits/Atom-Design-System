5d5e469cef7766efb1115e1c319302808b544516
Ruben <rubenabelyan.bangits@gmail.com>
2021-10-19 13:02:44 +0400

    commit for pull

--- src/components/pages/sign-in/SignIn.tsx
+++ src/components/pages/sign-in/SignIn.tsx
@@ -1,6 +1,6 @@
 import { PasswordInput } from '@/components/molecules';
 import { typedMemo } from '@/helpers';
-import { Button, ButtonProps, TextInput, TextInputProps, Typography } from '@my-ui/core';
+import { Button, TextInput, TextInputProps, Typography } from '@my-ui/core';
 import React, { FC, useCallback, useMemo } from 'react';
 import styles from './SignIn.module.scss';
 
@@ -12,7 +12,7 @@ export interface SignInProps {
   loginErrorMessage?: string;
   usernameInputLabel: string;
   passwordInputLabel: string;
-  buttonProps?: ButtonProps;
+  buttonProps?: string;
 }
 
 const SignIn: FC<SignInProps> = ({
