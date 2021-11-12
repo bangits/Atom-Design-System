86d987b7dc4a2e88f993eb01515cd92c980d7e22
ShavarshMartirosyan <shavarshmartirosyan.bangits@gmail.com>
2021-11-12 14:21:30 +0400

    added avatar card in players details page

--- src/components/pages/player-details/PlayerDetails.stories.tsx
+++ src/components/pages/player-details/PlayerDetails.stories.tsx
@@ -1,7 +1,7 @@
 import { EditedForm } from '@/components';
-import { Card, IconButton, Tab } from '@my-ui/core';
+import { Card, IconButton, SubTab, Tab } from '@my-ui/core';
 import { withKnobs } from '@storybook/addon-knobs';
-import React from 'react';
+import React, { useState } from 'react';
 import PlayerDetails from './PlayerDetails';
 
 export default {
@@ -11,6 +11,9 @@ export default {
 };
 
 export const Default = () => {
+  const [tabValue, setTabValue] = useState(1);
+  const [subTabValue, setSubTabValue] = useState(1);
+
   return (
     <PlayerDetails
       userInfo={{
@@ -90,220 +93,238 @@ export const Default = () => {
               }
             ]}
             defaultValue={1}
+            onChange={(value) => setTabValue(value)}
           />
-        </div>
-        <div style={{ display: 'flex', width: 986, paddingTop: 8 }}>
-          <div style={{ marginLeft: 16, marginBottom: 8 }}>
-            <div style={{ width: 477, marginBottom: 8 }}>
-              <EditedForm
+          <div style={{ paddingTop: 24, paddingLeft: 16, paddingRight: 16 }}>
+            {tabValue === 1 && (
+              <SubTab
                 options={[
-                  { title: 'Email', value: 'antonian.evgenia@gmail.com' },
-                  {
-                    title: 'Username',
-                    value: 'Eva123456'
-                  },
-                  {
-                    title: 'Currency',
-                    value: 'USD'
-                  },
-                  {
-                    title: 'Status',
-                    value: 'Active'
-                  },
-                  {
-                    title: 'Segment',
-                    value: 'Vip Players'
-                  },
-                  {
-                    title: 'Btag',
-                    value: '111.123.56.85'
-                  },
-                  {
-                    title: 'Registration Source',
-                    value: '09/05/2021 12:00:00'
-                  },
-                  {
-                    title: 'Created By',
-                    value: 'Inesa Khachatryan'
-                  },
-                  {
-                    title: 'Registration source ',
-                    value: '09/05/2021 12:00:00'
-                  },
-                  {
-                    title: 'Registration Form IP',
-                    value: 'No'
-                  },
-                  {
-                    title: 'Test Player',
-                    value: 'Yes'
-                  },
-                  {
-                    title: 'State',
-                    value: 'Forever'
-                  }
+                  { title: 'Player Details', value: 1, badgeCount: 0 },
+                  { title: 'Player KPIs', value: 2, badgeCount: 0 },
+                  { title: 'Documents', value: 3, badgeCount: 0 },
+                  { title: 'Messages', value: 4, badgeCount: 130 },
+                  { title: 'Notes', value: 5, badgeCount: 32 }
                 ]}
-                title='Account Information'
-                editButton={
-                  <IconButton
-                    icon={
-                      <svg xmlns='http://www.w3.org/2000/svg' width='12' height='12' viewBox='0 0 24 24'>
-                        <path
-                          fill='currentColor'
-                          d='M19.769 9.923l-12.642 12.639-7.127 1.438 1.438-7.128 12.641-12.64 5.69 5.691zm1.414-1.414l2.817-2.82-5.691-5.689-2.816 2.817 5.69 5.692z'
-                        />
-                      </svg>
-                    }
-                    variant='dark'
-                  />
-                }
+                defaultValue={subTabValue}
+                onChange={(value) => setSubTabValue(value)}
               />
-            </div>
-            <div style={{ width: 477, marginBottom: 8 }}>
-              <EditedForm
-                options={[
-                  {
-                    title: 'First Name',
-                    value: 'N/A'
-                  },
-                  {
-                    title: 'Last Name',
-                    value: 'Antonian'
-                  },
-                  {
-                    title: 'Middle Name',
-                    value: 'Sergei'
-                  },
-                  {
-                    title: 'Date Of Birth',
-                    value: '30/08/1995'
-                  },
-                  {
-                    title: 'Gender',
-                    value: 'Male'
-                  },
-                  {
-                    title: 'Document Type',
-                    value: 'Passport'
-                  },
-                  {
-                    title: 'Passport ID',
-                    value: '123456789'
-                  }
-                ]}
-                title='Personal Information'
-                editButton={
-                  <IconButton
-                    icon={
-                      <svg xmlns='http://www.w3.org/2000/svg' width='12' height='12' viewBox='0 0 24 24'>
-                        <path
-                          fill='currentColor'
-                          d='M19.769 9.923l-12.642 12.639-7.127 1.438 1.438-7.128 12.641-12.64 5.69 5.691zm1.414-1.414l2.817-2.82-5.691-5.689-2.816 2.817 5.69 5.692z'
-                        />
-                      </svg>
-                    }
-                    variant='dark'
-                  />
-                }
-              />
-            </div>
-          </div>
-          <div style={{ marginLeft: 8, marginBottom: 8 }}>
-            <div style={{ width: 477, marginBottom: 8 }}>
-              <EditedForm
-                options={[
-                  {
-                    title: 'Phone Number',
-                    value: '+374 (10) 11 00 13'
-                  },
-                  {
-                    title: 'Mobile Number',
-                    value: '+374 98 98 98 98'
-                  },
-                  {
-                    title: 'Zip Code',
-                    value: '1111'
-                  },
-                  {
-                    title: 'Address',
-                    value: '13 Qochar Street'
-                  },
-                  {
-                    title: 'Country',
-                    value: 'Armenia'
-                  },
-                  {
-                    title: 'City',
-                    value: 'Yerevan'
-                  },
-                  {
-                    title: 'Region',
-                    value: 'Armavir'
-                  },
-                  {
-                    title: 'Time Zone',
-                    value: '+4 GMT'
-                  }
-                ]}
-                title='Contacts'
-                editButton={
-                  <IconButton
-                    icon={
-                      <svg xmlns='http://www.w3.org/2000/svg' width='12' height='12' viewBox='0 0 24 24'>
-                        <path
-                          fill='currentColor'
-                          d='M19.769 9.923l-12.642 12.639-7.127 1.438 1.438-7.128 12.641-12.64 5.69 5.691zm1.414-1.414l2.817-2.82-5.691-5.689-2.816 2.817 5.69 5.692z'
-                        />
-                      </svg>
-                    }
-                    variant='dark'
-                  />
-                }
-              />
-            </div>
-            <div style={{ width: 477, marginBottom: 8 }}>
-              <EditedForm
-                options={[
-                  { title: 'Document Verified', value: 'Yes' },
-                  {
-                    title: 'Document Verified Date',
-                    value: '09/05/2021 12:00:00'
-                  },
-                  {
-                    title: 'Email Verified',
-                    value: 'Yes'
-                  },
-                  {
-                    title: 'Email Verified Date',
-                    value: '09/05/2021 12:00:00'
-                  },
-                  {
-                    title: 'Mobile Number Verified',
-                    value: 'Yes'
-                  },
-                  {
-                    title: 'Mobile Number Verified Date',
-                    value: '09/05/2021 12:00:00'
-                  }
-                ]}
-                title='Verifications'
-                editButton={
-                  <IconButton
-                    icon={
-                      <svg xmlns='http://www.w3.org/2000/svg' width='12' height='12' viewBox='0 0 24 24'>
-                        <path
-                          fill='currentColor'
-                          d='M19.769 9.923l-12.642 12.639-7.127 1.438 1.438-7.128 12.641-12.64 5.69 5.691zm1.414-1.414l2.817-2.82-5.691-5.689-2.816 2.817 5.69 5.692z'
-                        />
-                      </svg>
-                    }
-                    variant='dark'
-                  />
-                }
-              />
-            </div>
+            )}
           </div>
         </div>
+        {tabValue === 1 && subTabValue === 1 && (
+          <div style={{ display: 'flex', width: 986, paddingTop: 8 }}>
+            <div style={{ marginLeft: 16, marginBottom: 8 }}>
+              <div style={{ width: 477, marginBottom: 8 }}>
+                <EditedForm
+                  options={[
+                    { title: 'Email', value: 'antonian.evgenia@gmail.com' },
+                    {
+                      title: 'Username',
+                      value: 'Eva123456'
+                    },
+                    {
+                      title: 'Currency',
+                      value: 'USD'
+                    },
+                    {
+                      title: 'Status',
+                      value: 'Active'
+                    },
+                    {
+                      title: 'Segment',
+                      value: 'Vip Players'
+                    },
+                    {
+                      title: 'Btag',
+                      value: '111.123.56.85'
+                    },
+                    {
+                      title: 'Registration Source',
+                      value: '09/05/2021 12:00:00'
+                    },
+                    {
+                      title: 'Created By',
+                      value: 'Inesa Khachatryan'
+                    },
+                    {
+                      title: 'Registration source ',
+                      value: '09/05/2021 12:00:00'
+                    },
+                    {
+                      title: 'Registration Form IP',
+                      value: 'No'
+                    },
+                    {
+                      title: 'Test Player',
+                      value: 'Yes'
+                    },
+                    {
+                      title: 'State',
+                      value: 'Forever'
+                    }
+                  ]}
+                  title='Account Information'
+                  editButton={
+                    <IconButton
+                      icon={
+                        <svg xmlns='http://www.w3.org/2000/svg' width='12' height='12' viewBox='0 0 24 24'>
+                          <path
+                            fill='currentColor'
+                            d='M19.769 9.923l-12.642 12.639-7.127 1.438 1.438-7.128 12.641-12.64 5.69 5.691zm1.414-1.414l2.817-2.82-5.691-5.689-2.816 2.817 5.69 5.692z'
+                          />
+                        </svg>
+                      }
+                      variant='dark'
+                    />
+                  }
+                />
+              </div>
+              <div style={{ width: 477, marginBottom: 8 }}>
+                <EditedForm
+                  options={[
+                    {
+                      title: 'First Name',
+                      value: 'N/A'
+                    },
+                    {
+                      title: 'Last Name',
+                      value: 'Antonian'
+                    },
+                    {
+                      title: 'Middle Name',
+                      value: 'Sergei'
+                    },
+                    {
+                      title: 'Date Of Birth',
+                      value: '30/08/1995'
+                    },
+                    {
+                      title: 'Gender',
+                      value: 'Male'
+                    },
+                    {
+                      title: 'Document Type',
+                      value: 'Passport'
+                    },
+                    {
+                      title: 'Passport ID',
+                      value: '123456789'
+                    }
+                  ]}
+                  title='Personal Information'
+                  editButton={
+                    <IconButton
+                      icon={
+                        <svg xmlns='http://www.w3.org/2000/svg' width='12' height='12' viewBox='0 0 24 24'>
+                          <path
+                            fill='currentColor'
+                            d='M19.769 9.923l-12.642 12.639-7.127 1.438 1.438-7.128 12.641-12.64 5.69 5.691zm1.414-1.414l2.817-2.82-5.691-5.689-2.816 2.817 5.69 5.692z'
+                          />
+                        </svg>
+                      }
+                      variant='dark'
+                    />
+                  }
+                />
+              </div>
+            </div>
+            <div style={{ marginLeft: 8, marginBottom: 8 }}>
+              <div style={{ width: 477, marginBottom: 8 }}>
+                <EditedForm
+                  options={[
+                    {
+                      title: 'Phone Number',
+                      value: '+374 (10) 11 00 13'
+                    },
+                    {
+                      title: 'Mobile Number',
+                      value: '+374 98 98 98 98'
+                    },
+                    {
+                      title: 'Zip Code',
+                      value: '1111'
+                    },
+                    {
+                      title: 'Address',
+                      value: '13 Qochar Street'
+                    },
+                    {
+                      title: 'Country',
+                      value: 'Armenia'
+                    },
+                    {
+                      title: 'City',
+                      value: 'Yerevan'
+                    },
+                    {
+                      title: 'Region',
+                      value: 'Armavir'
+                    },
+                    {
+                      title: 'Time Zone',
+                      value: '+4 GMT'
+                    }
+                  ]}
+                  title='Contacts'
+                  editButton={
+                    <IconButton
+                      icon={
+                        <svg xmlns='http://www.w3.org/2000/svg' width='12' height='12' viewBox='0 0 24 24'>
+                          <path
+                            fill='currentColor'
+                            d='M19.769 9.923l-12.642 12.639-7.127 1.438 1.438-7.128 12.641-12.64 5.69 5.691zm1.414-1.414l2.817-2.82-5.691-5.689-2.816 2.817 5.69 5.692z'
+                          />
+                        </svg>
+                      }
+                      variant='dark'
+                    />
+                  }
+                />
+              </div>
+              <div style={{ width: 477, marginBottom: 8 }}>
+                <EditedForm
+                  options={[
+                    { title: 'Document Verified', value: 'Yes' },
+                    {
+                      title: 'Document Verified Date',
+                      value: '09/05/2021 12:00:00'
+                    },
+                    {
+                      title: 'Email Verified',
+                      value: 'Yes'
+                    },
+                    {
+                      title: 'Email Verified Date',
+                      value: '09/05/2021 12:00:00'
+                    },
+                    {
+                      title: 'Mobile Number Verified',
+                      value: 'Yes'
+                    },
+                    {
+                      title: 'Mobile Number Verified Date',
+                      value: '09/05/2021 12:00:00'
+                    }
+                  ]}
+                  title='Verifications'
+                  editButton={
+                    <IconButton
+                      icon={
+                        <svg xmlns='http://www.w3.org/2000/svg' width='12' height='12' viewBox='0 0 24 24'>
+                          <path
+                            fill='currentColor'
+                            d='M19.769 9.923l-12.642 12.639-7.127 1.438 1.438-7.128 12.641-12.64 5.69 5.691zm1.414-1.414l2.817-2.82-5.691-5.689-2.816 2.817 5.69 5.692z'
+                          />
+                        </svg>
+                      }
+                      variant='dark'
+                    />
+                  }
+                />
+              </div>
+            </div>
+          </div>
+        )}
       </Card>
     </PlayerDetails>
   );
--- src/components/pages/player-details/PlayerDetails.tsx
+++ src/components/pages/player-details/PlayerDetails.tsx
@@ -1,5 +1,5 @@
 import { typedMemo } from '@/helpers';
-import { Breadcrumb, Button, Card, Status, StatusProps, Tag } from '@my-ui/core';
+import { AvatarCard, Breadcrumb, Button, Card, Status, StatusProps, Tag } from '@my-ui/core';
 import classNames from 'classnames';
 import React, { FC } from 'react';
 import styles from './PlayerDetails.module.scss';
@@ -69,7 +69,13 @@ const PlayerDetails: FC<PlayerDetailsProps> = ({ children, userInfo, walletInfo,
         <div className={classNames(styles['PlayerDetailsUserInfo--content-info'])}>
           <Card borderRadius={1.6} className={classNames(styles['PlayerDetailsUserInfo--user'])}>
             <div className={classNames(styles['PlayerDetailsUserInfo--user-detailsContainer'])}>
-              <div className={classNames(styles['PlayerDetailsUserInfo--user-avatarContainer'])}></div>
+              <div className={classNames(styles['PlayerDetailsUserInfo--user-avatarContainer'])}>
+                <AvatarCard
+                  avatarImg='https://cdn.celebsagewiki.com/images/22/tatul-avoyan.jpg'
+                  imageSize='sm'
+                  variant='online'
+                />
+              </div>
               <div className={classNames(styles['PlayerDetailsUserInfo--user-details'])}>
                 <span>{userInfo.title}</span>
                 <span>{userInfo.id}</span>
@@ -89,7 +95,7 @@ const PlayerDetails: FC<PlayerDetailsProps> = ({ children, userInfo, walletInfo,
               </div>
               <ul>
                 {walletInfo.realBalance.subBalance.map((sub) => (
-                  <li>
+                  <li key={sub.label}>
                     <span>{sub.label}</span>
                     <span>{sub.balance}</span>
                   </li>
@@ -115,7 +121,7 @@ const PlayerDetails: FC<PlayerDetailsProps> = ({ children, userInfo, walletInfo,
             <span>{lastActivity.title}</span>
             <div className={classNames(styles['PlayerDetailsLastActivity--container'])}>
               {lastActivity.info.map((i) => (
-                <div>
+                <div key={i.label}>
                   <span className={classNames(styles['PlayerDetailsLastActivity--label'])}>{i.label}</span>
                   <span className={classNames(styles['PlayerDetailsLastActivity--date'])}>{i.date}</span>
                 </div>
@@ -127,7 +133,7 @@ const PlayerDetails: FC<PlayerDetailsProps> = ({ children, userInfo, walletInfo,
             <span className={classNames(styles['PlayerDetailsTags--title'])}>Tags</span>
             <div className={classNames(styles['PlayerDetailsTags--container'])}>
               <div>
-                <Tag title='Lorem Ip' closeIcon className={classNames(styles['PlayerDetailsTags--tag'])} />
+                <Tag title='Lorem Ip' closeIcon className={styles['PlayerDetailsTags--tag']} />
               </div>
               <div>
                 <Tag title='Lorem Ip' closeIcon className={classNames(styles['PlayerDetailsTags--tag'])} />
@@ -136,7 +142,21 @@ const PlayerDetails: FC<PlayerDetailsProps> = ({ children, userInfo, walletInfo,
                 <Tag title='Lorem Ip' closeIcon className={classNames(styles['PlayerDetailsTags--tag'])} />
               </div>
               <div>
-                <Button variant='ghost' className={styles['PlayerDetailsTags--button']}>
+                <Button
+                  variant='ghost'
+                  className={styles['PlayerDetailsTags--button']}
+                  startIcon={
+                    <svg xmlns='http://www.w3.org/2000/svg' width='9.424' height='9.424' viewBox='0 0 9.424 9.424'>
+                      <g id='Group_23677' data-name='Group 23677' transform='translate(-717.284 -219.283)'>
+                        <path
+                          id='close'
+                          d='M3.943,3.332,6.537.737a.432.432,0,0,0-.61-.61L3.332,2.722.737.127a.432.432,0,0,0-.61.61L2.722,3.332.127,5.927a.432.432,0,1,0,.61.61L3.332,3.943,5.927,6.537a.432.432,0,0,0,.61-.61Zm0,0'
+                          transform='translate(717.284 223.996) rotate(-45)'
+                          fill='#3C54B2'
+                        />
+                      </g>
+                    </svg>
+                  }>
                   Add New
                 </Button>
               </div>
