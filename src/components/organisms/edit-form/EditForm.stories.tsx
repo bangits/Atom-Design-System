27a81ae4036c4316820a1ec4c764f6307d9298b8
ShavarshMartirosyan <shavarshmartirosyan.bangits@gmail.com>
2021-11-15 16:14:54 +0400

    added player details edit

--- src/components/organisms/edit-form/EditForm.stories.tsx
+++ src/components/organisms/edit-form/EditForm.stories.tsx
@@ -1,3 +1,4 @@
+import { IconButton } from '@my-ui/core';
 import { withKnobs } from '@storybook/addon-knobs';
 import React from 'react';
 import EditForm from './EditForm';
@@ -10,8 +11,123 @@ export default {
 
 export const Default = () => {
   return (
-    <div style={{ width: 600 }}>
-      <EditForm />
+    <div style={{ width: 477 }}>
+      <EditForm
+        title='Account Information'
+        applyButton={
+          <IconButton
+            icon={
+              <svg id='done_black_24dp' xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'>
+                <path id='Path_43198' data-name='Path 43198' d='M0,0H24V24H0Z' fill='none' />
+                <path
+                  id='Path_43199'
+                  data-name='Path 43199'
+                  d='M8.491,15.236,4.673,11.418,3.4,12.691l5.091,5.091L19.4,6.873,18.127,5.6Z'
+                  transform='translate(1)'
+                  fill='#00b21e'
+                />
+              </svg>
+            }
+          />
+        }
+        closeButton={
+          <IconButton
+            icon={
+              <svg id='close_black_24dp' xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'>
+                <path id='Path_43196' data-name='Path 43196' d='M0,0H24V24H0Z' fill='none' />
+                <path
+                  id='Path_43197'
+                  data-name='Path 43197'
+                  d='M17,6.209,15.791,5,11,9.791,6.209,5,5,6.209,9.791,11,5,15.791,6.209,17,11,12.209,15.791,17,17,15.791,12.209,11Z'
+                  transform='translate(1 1)'
+                  fill='#505d6e'
+                />
+              </svg>
+            }
+          />
+        }
+        renderInputs={(Component, name) => {
+          return <Component />;
+        }}
+        fields={[
+          {
+            type: 'input',
+            name: 'name',
+            props: {
+              label: 'Provider Name'
+            }
+          },
+          {
+            type: 'select',
+            name: 'targetMarkets',
+            props: {
+              inputLabel: 'Target market',
+              options: [
+                { label: 'market 1', value: 1 },
+                { label: 'market 2', value: 2 },
+                { label: 'market 3', value: 3 }
+              ],
+              isSearchable: true,
+              isMulti: true
+            }
+          },
+          {
+            type: 'select',
+            name: 'certifiedCountries',
+            props: {
+              inputLabel: 'Certified countries',
+              options: [
+                { label: 'Certified country 1', value: 1 },
+                { label: 'Certified country 2', value: 2 },
+                { label: 'Certified country 3', value: 3 }
+              ],
+              isSearchable: true,
+              isMulti: true
+            }
+          },
+          {
+            type: 'select',
+            name: 'certifiedCountries',
+            props: {
+              inputLabel: 'Restricted countries',
+              options: [
+                { label: 'Restricted country 1', value: 1 },
+                { label: 'Restricted country 2', value: 2 },
+                { label: 'Restricted country 3', value: 3 }
+              ],
+              isSearchable: true,
+              isMulti: true
+            }
+          },
+          {
+            type: 'select',
+            name: 'providerCurrencies',
+            props: {
+              inputLabel: 'Currency',
+              options: [
+                { label: 'Currency 1', value: 1 },
+                { label: 'Currency 2', value: 2 },
+                { label: 'Currency 3', value: 3 }
+              ],
+              isSearchable: true,
+              isMulti: true
+            }
+          },
+          {
+            type: 'select',
+            name: 'defaultCurrency',
+            props: {
+              inputLabel: 'Default currency',
+              options: [
+                { label: 'Default currency 1', value: 1 },
+                { label: 'Default currency 2', value: 2 },
+                { label: 'Default currency 3', value: 3 }
+              ],
+              isSearchable: true
+            }
+          }
+        ]}
+      />
     </div>
   );
 };
--- src/components/organisms/edit-form/EditForm.tsx
+++ src/components/organisms/edit-form/EditForm.tsx
@@ -1,13 +1,92 @@
 import { typedMemo } from '@/helpers';
-import { Card } from '@my-ui/core';
+import { Card, Select, SelectProps, TextInput, TextInputProps } from '@my-ui/core';
 import classNames from 'classnames';
-import React from 'react';
+import React, { FC, ReactNode } from 'react';
 import styles from './EditForm.module.scss';
 
-const EditForm = () => {
+export type FormSelectProps = SelectProps<any[], boolean, any>;
+export interface EditFormProps {
+  title: string;
+  applyButton: ReactNode;
+  closeButton: ReactNode;
+  renderInputs: (
+    InputComponent: React.ComponentType,
+    name: string,
+    fieldType: 'select' | 'input' | 'custom'
+  ) => JSX.Element;
+  fields: Array<
+    {
+      name: string;
+    } & (
+      | {
+          type: 'select';
+          disabled?: boolean;
+          props?: FormSelectProps;
+          component?: (props: FormSelectProps) => JSX.Element;
+        }
+      | {
+          type: 'input';
+          disabled?: boolean;
+          props?: TextInputProps;
+          component?: (props: TextInputProps) => JSX.Element;
+        }
+      | {
+          type: 'custom';
+          disabled?: boolean;
+          component: () => JSX.Element;
+        }
+    )
+  >;
+}
+
+const EditForm: FC<EditFormProps> = ({ title, applyButton, closeButton, fields, renderInputs }) => {
   return (
     <Card borderRadius={1.6} className={classNames(styles.EditFormBase)}>
-      <div className={classNames(styles['EditFormBase'])}></div>
+      <div className={classNames(styles['EditFormBase--header'])}>
+        <span>{title}</span>
+        <div className={classNames(styles['EditFormBase--buttons'])}>
+          {applyButton}
+          {closeButton}
+        </div>
+      </div>
+      <div className={classNames(styles['EditFormBase--content'])}>
+        {fields.map((field, idx) => (
+          <div className={styles['EditFormBase--field']}>
+            {field.type === 'custom' ? (
+              field.component()
+            ) : (
+              <>
+                {field.component
+                  ? //@ts-ignore
+                    renderInputs(field.component, field.name, field.type)
+                  : renderInputs(
+                      (props: typeof field.props) => (
+                        <>
+                          {field.type === 'input' ? (
+                            <TextInput
+                              key={idx}
+                              disabled={field.disabled}
+                              {...(props as TextInputProps)}
+                              {...field.props}
+                            />
+                          ) : (
+                            <Select
+                              disabled={field.disabled}
+                              {...(props as SelectProps<any, any, any>)}
+                              {...field.props}
+                              fullWidth
+                            />
+                          )}
+                        </>
+                      ),
+                      field.name,
+                      field.type
+                    )}
+              </>
+            )}
+          </div>
+        ))}
+      </div>
     </Card>
   );
 };
