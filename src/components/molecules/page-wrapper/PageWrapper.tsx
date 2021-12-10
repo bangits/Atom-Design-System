a13cc9d864b66eedd48dca73afb5100f9b1286eb
Armen Mesropyan <amesropyan@bangits.com>
2021-12-10 16:54:10 +0400

    Fixed PageWrapper condition

--- src/components/molecules/page-wrapper/PageWrapper.tsx
+++ src/components/molecules/page-wrapper/PageWrapper.tsx
@@ -12,15 +12,14 @@ export interface PageWrapperProps {
 const PageWrapper: FC<PageWrapperProps> = ({ children, title, showButton, buttonProps = {} }) => {
   return (
     <div className={styles.PageWrapper}>
-      {title ||
-        (showButton && (
-          <div className={styles.PageWrapperHeader}>
-            <Typography component='h2' variant='h2' className={styles.PageTitle}>
-              {title}
-            </Typography>
-            {showButton && <Button {...buttonProps} type='button' />}
-          </div>
-        ))}
+      {(title || showButton) && (
+        <div className={styles.PageWrapperHeader}>
+          <Typography component='h2' variant='h2' className={styles.PageTitle}>
+            {title}
+          </Typography>
+          {showButton && <Button {...buttonProps} type='button' />}
+        </div>
+      )}
 
       {children}
     </div>
