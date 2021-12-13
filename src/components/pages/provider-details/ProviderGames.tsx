bdedac1c51dd6e9c20686a8fad441b39626723c6
Armen Mesropyan <amesropyan@bangits.com>
2021-12-13 13:34:33 +0400

    Commit for pull

--- src/components/pages/provider-details/ProviderGames.tsx
+++ src/components/pages/provider-details/ProviderGames.tsx
@@ -44,6 +44,9 @@ export const ProviderGames = ({ translations, gameTypes, games, onGameClick }: P
                 <path d='m503.6 463.2-96.7-96.4C438.1 327.1 455 278 455 227.5 455 101.8 353.1 0 227.5 0 101.8 0 0 101.8 0 227.5 0 353.1 101.8 455 227.5 455c50.5.1 99.6-16.9 139.3-48.1l96.4 96.7c11.1 11.1 29.1 11.2 40.2.2l.2-.2c11.1-11.1 11.2-29.1.2-40.2l-.2-.2zM56.9 227.5c0-94.2 76.4-170.6 170.6-170.6 94.2 0 170.6 76.4 170.6 170.6 0 94.2-76.4 170.6-170.6 170.6-94.3 0-170.6-76.4-170.6-170.6z' />
               </svg>
             }
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+            maxLength={30}
           />
         </div>
         <div className={classNames(styles['GamesList__Header-2'], 'GamesList__Header-2')}>
@@ -56,36 +59,38 @@ export const ProviderGames = ({ translations, gameTypes, games, onGameClick }: P
         </div>
       </div>
 
-      <div className={classNames(styles['GamesList__Empty'], 'GamesList__Empty')}>
-        <div className={classNames(styles['Empty-Page-Cell'], 'Empty-Page-Cell')}>
-          <span className={classNames(styles['Empty-Page-Icon-Cell'], 'Empty-Page-Icon-Cell')}>
-            <EmptyGameListIcon />
-          </span>
+      {games && games.length ? (
+        <div className={classNames(styles['GamesList__Fill'], 'GamesList__Fill')}>
+          <div className={classNames(styles['TagsCell'], 'TagsCell')}>
+            {gameTypes.map((type) => (
+              <Tag
+                className={classNames(styles['GameDetailsBase__GameTag'], 'GameDetailsBase__GameTag')}
+                title={`${type.name}(${type.gameCount})`}
+                closeIcon={false}
+                inactive={selectedGameType !== type.id}
+                key={type.id}
+                onClick={() => setSelectedGameType(type.id)}
+              />
+            ))}
+          </div>
+          <div className={classNames(styles['GamesContainer'], 'GamesContainer')}>
+            {games.map((game) => (
+              <CardImg title={game.name} image={game.icon} key={game.id} handleClick={() => onGameClick(game.id)} />
+            ))}
+          </div>
         </div>
-        <Typography component='p' variant='p3' className={classNames(styles['Empty-Page-Title'], 'Empty-Page-Title')}>
-          {translations.noGames}
-        </Typography>
-      </div>
-
-      <div className={classNames(styles['GamesList__Fill'], 'GamesList__Fill')}>
-        <div className={classNames(styles['TagsCell'], 'TagsCell')}>
-          {gameTypes.map((type) => (
-            <Tag
-              className={classNames(styles['GameDetailsBase__GameTag'], 'GameDetailsBase__GameTag')}
-              title={`${type.name}(${type.gameCount})`}
-              closeIcon={false}
-              inactive={selectedGameType !== type.id}
-              key={type.id}
-              onClick={() => setSelectedGameType(type.id)}
-            />
-          ))}
+      ) : (
+        <div className={classNames(styles['GamesList__Empty'], 'GamesList__Empty')}>
+          <div className={classNames(styles['Empty-Page-Cell'], 'Empty-Page-Cell')}>
+            <span className={classNames(styles['Empty-Page-Icon-Cell'], 'Empty-Page-Icon-Cell')}>
+              <EmptyGameListIcon />
+            </span>
+          </div>
+          <Typography component='p' variant='p3' className={classNames(styles['Empty-Page-Title'], 'Empty-Page-Title')}>
+            {translations.noGames}
+          </Typography>
         </div>
-        <div className={classNames(styles['GamesContainer'], 'GamesContainer')}>
-          {games.map((game) => (
-            <CardImg title={game.name} image={game.icon} key={game.id} handleClick={() => onGameClick(game.id)} />
-          ))}
-        </div>
-      </div>
+      )}
     </div>
   );
 };
