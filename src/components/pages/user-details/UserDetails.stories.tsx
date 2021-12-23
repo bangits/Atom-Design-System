d7d9e1a1906901ab57b7f72a7e269cb326559de7
Armen Mesropyan <amesropyan@bangits.com>
2021-12-23 21:12:07 +0400

    Commit for pull

--- src/components/pages/user-details/UserDetails.stories.tsx
+++ src/components/pages/user-details/UserDetails.stories.tsx
@@ -10,6 +10,7 @@ export default {
 export const Default = () => {
   return (
     <UserDetails
+      walletTabContent={<></>}
       createdBy={'11/11/1111'}
       creationDate={'2017-11/11'}
       breadCrumbs={[
