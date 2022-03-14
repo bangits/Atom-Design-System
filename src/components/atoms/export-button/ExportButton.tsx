4657bdf12270b2c3812313807166312d5c866b63
Ruben <ru.abelyan@mail.ru>
2022-03-14 11:47:48 +0400

    fixed text

--- src/components/atoms/export-button/ExportButton.tsx
+++ src/components/atoms/export-button/ExportButton.tsx
@@ -5,5 +5,5 @@ import { FC } from 'react';
 export type ExportButtonProps = Omit<ButtonProps, 'startIcon' | 'variant'>;
 
 export const ExportButton: FC<ExportButtonProps> = (props) => {
-  return <Button {...props} startIcon={<ExportIcon />} variant='ghost' />;
+  return <Button {...props} startIcon={<ExportIcon width='15px' height='15px' />} variant='ghost' />;
 };
