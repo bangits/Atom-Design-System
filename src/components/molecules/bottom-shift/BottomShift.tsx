9efb9b5a4bd0bba21c3410c17ede5f8f518748aa
Armen Mesropyan <amesropyan@bangits.com>
2022-08-08 20:21:25 +0400

    Update .npmrc

--- src/components/molecules/bottom-shift/BottomShift.tsx
+++ src/components/molecules/bottom-shift/BottomShift.tsx
@@ -14,23 +14,31 @@ const BottomShift: FC<PropsWithChildren<BottomShiftProps>> = ({ children, opened
   return (
     <CSSTransition in={opened} timeout={300} unmountOnExit>
       {(status) => {
+        const isOpened = status !== 'exited' && status !== 'exiting';
+
         return (
           <>
             <div
               role='button'
               className={classNames(styles['BottomShift__blur'], {
-                [styles['BottomShift__blur--open']]: status !== 'exited' && status !== 'exiting'
+                [styles['BottomShift__blur--open']]: isOpened
               })}
               onClick={onClose}
             />
 
             <Card
               className={classNames(styles.BottomShift, {
-                [styles['BottomShift--open']]: status !== 'exited' && status !== 'exiting'
+                [styles['BottomShift--open']]: isOpened
               })}>
               {children}
 
-              <IconButton onClick={onClose} icon={<Icons.CloseIcon />} className={styles['BottomShift__close']} />
+              <IconButton
+                onClick={onClose}
+                icon={<Icons.CloseIcon />}
+                className={classNames(styles['BottomShift__close'], {
+                  [styles['BottomShift__close--open']]: isOpened
+                })}
+              />
             </Card>
           </>
         );
