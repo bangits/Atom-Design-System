e3ff7ee870b07a5587fd30f537dfe546dc0ed65b
Ruben <ru.abelyan@mail.ru>
2022-08-09 15:29:08 +0400

    fix id bug in main information section

--- src/components/molecules/main-information/MainInformation.tsx
+++ src/components/molecules/main-information/MainInformation.tsx
@@ -19,10 +19,12 @@ const MainInformation: FC<MainInformationProps> = ({ addSpacings = true, name, i
         [styles['MainInformation--with-spacings']]: addSpacings
       })}>
       <span className={styles['MainInformation__icon']}>{icon}</span>
-      <div className={styles['MainInformation__informations']}>
+      <div className={styles['MainInformation__informations']} style={{}}>
         <span className={styles['MainInformation__name']}>{name}</span>
-        <div className={styles['MainInformation__id']}>
-          {idLabel} {id}
+        <div className={styles['MainInformation__id_block']}>
+          <div className={styles['MainInformation__id']}>
+            {idLabel} {id}
+          </div>
           {addCopyButton && <CopyButton copyText={id} />}
         </div>
       </div>
