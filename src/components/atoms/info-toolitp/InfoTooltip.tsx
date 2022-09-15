1dfa70bb479967e7a736d7d96231d321e1c928bb
Ruben <ru.abelyan@mail.ru>
2022-09-15 17:36:45 +0400

    add components

--- src/components/atoms/info-toolitp/InfoTooltip.tsx
+++ src/components/atoms/info-toolitp/InfoTooltip.tsx
@@ -3,12 +3,13 @@ import { Tooltip } from '@my-ui/core';
 import { FC } from 'react';
 import styles from './InfoTooltip.module.scss';
 
-export const InfoTooltip: FC<{ infoTooltipText?: string }> = ({ infoTooltipText }) => {
+export const InfoTooltip: FC<{ infoTooltipText?: string; isQuestion?: boolean }> = ({
+  infoTooltipText,
+  isQuestion
+}) => {
   return (
     <Tooltip showEvent='hover' text={infoTooltipText} disabled={!infoTooltipText}>
-      <div className={styles.InfoIcon}>
-        <Icons.InfoTooltipIcon />
-      </div>
+      <div className={styles.InfoIcon}>{isQuestion ? <Icons.QuestionIcon /> : <Icons.InfoTooltipIcon />}</div>
     </Tooltip>
   );
 };
