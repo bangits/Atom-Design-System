82409095d7dfebe1ccb4301ceca7d5e038b9263b
Ruben <ru.abelyan@mail.ru>
2022-11-08 18:47:52 +0400

    fix field changes

--- src/components/molecules/table-field/TableField.tsx
+++ src/components/molecules/table-field/TableField.tsx
@@ -6,16 +6,18 @@ import { FC } from 'react';
 export interface TableFieldProps extends DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
   backgroundColor?: string;
   writeMode?: boolean;
+  color?: string;
 }
 
-export const TableField: FC<TableFieldProps> = ({ backgroundColor, writeMode, ...props }) => {
+export const TableField: FC<TableFieldProps> = ({ backgroundColor, writeMode, color, ...props }) => {
   return (
     <input
       style={backgroundColor && { background: backgroundColor }}
       autoComplete='off'
       className={classNames(styles.Field, {
         [styles[`Field--disabled`]]: props.disabled,
-        [styles[`Field--write-mode`]]: writeMode
+        [styles[`Field--write-mode`]]: writeMode,
+        [styles[`Field--${color}`]]: color
       })}
       {...props}
     />
