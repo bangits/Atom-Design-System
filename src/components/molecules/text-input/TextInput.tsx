2a0d4b26b53c33ffb145a7d9f2187955104181df
Armen Mesropyan <amesropyan@bangits.com>
2022-12-15 14:32:33 +0400

    Fix FromToInput

--- src/components/molecules/text-input/TextInput.tsx
+++ src/components/molecules/text-input/TextInput.tsx
@@ -1,6 +1,7 @@
 import { Icons } from '@/atom-design-system';
 import { Label } from '@/components';
 import { TextInput as MYUITextInput, TextInputProps as MyUITextInputProps, Tooltip } from '@my-ui/core';
+import classNames from 'classnames';
 import { ReactNode } from 'react';
 import styles from './TextInput.module.scss';
 
@@ -13,11 +14,11 @@ export interface TextInputDesignSystemProps extends MyUITextInputProps {
   };
 }
 
-export const TextInput = ({ tooltipProps, ...props }: TextInputDesignSystemProps) => {
+export const TextInput = ({ tooltipProps, containerClassName, ...props }: TextInputDesignSystemProps) => {
   const TooltipIcon = tooltipProps && Icons[tooltipProps.icon || 'InfoTooltipIcon'];
 
   return (
-    <div className={styles.TextInputContainer}>
+    <div className={classNames(styles.TextInputContainer, containerClassName)}>
       {tooltipProps && (
         <Tooltip showEvent='hover' text={tooltipProps.tooltipText} placement='top'>
           <div className={styles.TextInputContainer__Tooltip}>
