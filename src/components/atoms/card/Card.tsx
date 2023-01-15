b248911a2f3fb10b1584adaf805fe57d59dbc903
GagikAnanikyan <gananikyan@bangits.com>
2023-01-15 22:12:42 +0400

    Commit for push

--- src/components/atoms/card/Card.tsx
+++ src/components/atoms/card/Card.tsx
@@ -1,4 +1,4 @@
-import { Icons } from '@/atom-design-system';
+import { Icons, Typography } from '@/atom-design-system';
 import { ComponentType } from '@my-ui/core/dist/types';
 import classNames from 'classnames';
 import { CSSProperties, FC, ReactNode, useCallback, useLayoutEffect, useRef, useState } from 'react';
@@ -10,9 +10,11 @@ export interface CardProps {
   actions?: ReactNode;
 
   height?: CSSProperties['height'];
+
+  index?: number;
 }
 
-const Card: FC<CardProps> = ({ component: Component = 'div', actions, height = '14.5rem', children }) => {
+const Card: FC<CardProps> = ({ component: Component = 'div', actions, height = '14.5rem', children, index }) => {
   const [openViewMoreCollapse, setOpenViewMoreCollapse] = useState(false);
 
   const cardContentElRef = useRef<HTMLDivElement | null>(null);
@@ -27,29 +29,36 @@ const Card: FC<CardProps> = ({ component: Component = 'div', actions, height = '
   }, [cardContentElRef]);
 
   return (
-    <Component
-      className={classNames(styles.Card, {
-        [styles['Card--WithActions']]: actions,
-        [styles['Card--CollapseOpened']]: openViewMoreCollapse,
-        [styles['Card--WithCollapse']]: showViewMoreBtnRef.current
-      })}>
-      <div
-        className={styles.Card__Content}
-        ref={cardContentElRef}
-        style={{ height: openViewMoreCollapse ? cardContentElRef.current?.scrollHeight : height }}>
-        {children}
-      </div>
-
-      <div className={styles.Card__Actions}>{actions}</div>
-
-      {showViewMoreBtnRef.current && (
-        <button className={styles.Card__Btn} type='button' onClick={toggleViewMoreCollapse}>
-          {openViewMoreCollapse ? 'View Less' : 'View More'}
-
-          <Icons.ViewMoreArrowIcon />
-        </button>
+    <div className={styles['Card--wrapper']}>
+      {index && (
+        <Typography variant='p4' className={styles['Card__index']}>
+          {index}
+        </Typography>
       )}
-    </Component>
+      <Component
+        className={classNames(styles.Card, {
+          [styles['Card--WithActions']]: actions,
+          [styles['Card--CollapseOpened']]: openViewMoreCollapse,
+          [styles['Card--WithCollapse']]: showViewMoreBtnRef.current
+        })}>
+        <div
+          className={styles.Card__Content}
+          ref={cardContentElRef}
+          style={{ height: openViewMoreCollapse ? cardContentElRef.current?.scrollHeight : height }}>
+          {children}
+        </div>
+
+        <div className={styles.Card__Actions}>{actions}</div>
+
+        {showViewMoreBtnRef.current && (
+          <button className={styles.Card__Btn} type='button' onClick={toggleViewMoreCollapse}>
+            {openViewMoreCollapse ? 'View Less' : 'View More'}
+
+            <Icons.ViewMoreArrowIcon />
+          </button>
+        )}
+      </Component>
+    </div>
   );
 };
 
