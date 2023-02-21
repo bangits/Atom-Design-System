f0ec4954986072f1bb114ad070484b7be55debcd
Armen Mesropyan <amesropyan@bangits.com>
2023-02-21 17:44:26 +0400

    Fixed profile block image position

--- src/components/molecules/profile-block/ProfileBlock.tsx
+++ src/components/molecules/profile-block/ProfileBlock.tsx
@@ -38,53 +38,55 @@ const ProfileBlock = ({
   return (
     <>
       <Card className={classNames(styles['ProfileBlock'], 'ProfileBlock')} borderRadius={1.6}>
-        {onMainImgClick !== undefined && (
-          <div
-            className={classNames(styles['ProfileBlock__ImgCell'], 'ProfileBlock__ImgCell', {
-              [styles['ProfileBlock__ImgCell--uploaded']]: !!mainImgUrl
-            })}
-            onClick={mainImgUrl ? undefined : onMainImgClick}>
-            {mainImgUrl && (
-              <img className={classNames(styles['ProfileBlock__Img'], 'ProfileBlock__Img')} src={mainImgUrl} />
-            )}
-
-            {!isLoadingImage && !viewMode && (
-              <PhotoCamIcon
-                onClick={!mainImgUrl ? undefined : onMainImgClick}
-                width='2.1rem'
-                className={classNames(styles['ProfileBlock__PhotoCamIcon'], 'ProfileBlock__PhotoCamIcon')}
-              />
-            )}
-          </div>
-        )}
-        <div
-          className={classNames(styles['ProfileBlock__Cover'], 'ProfileBlock__Cover', {
-            [styles['ProfileBlock__Cover--uploaded']]: !!backgroundImgUrl
-          })}
-          onClick={backgroundImgUrl ? undefined : onBackgroundImgClick}>
-          {backgroundImgUrl && (
-            <img
-              className={classNames(styles['ProfileBlock__CoverImg'], 'ProfileBlock__CoverImg', {
-                [styles['ProfileBlock__Image']]:
-                  backgroundImgUrl === 'https://nid.com.ua/wp-content/themes/realestate-7/images/no-image.png' ||
-                  backgroundImgUrl === 'https://i.ibb.co/dMQPWnZ/no-image.png' ||
-                  backgroundImgUrl === 'https://i.ibb.co/c2yqT5q/image.png'
-              })}
-              src={backgroundImgUrl}
-            />
-          )}
-          {!viewMode && (
+        <div className={styles['ProfileBlock__Images']}>
+          {onMainImgClick !== undefined && (
             <div
-              className={classNames(styles['ProfileBlock__CoverPhotoIconCell'], 'ProfileBlock__CoverPhotoIconCell')}
-              onClick={!backgroundImgUrl ? undefined : onBackgroundImgClick}>
-              {!isLoadingImage && (
+              className={classNames(styles['ProfileBlock__ImgCell'], 'ProfileBlock__ImgCell', {
+                [styles['ProfileBlock__ImgCell--uploaded']]: !!mainImgUrl
+              })}
+              onClick={mainImgUrl ? undefined : onMainImgClick}>
+              {mainImgUrl && (
+                <img className={classNames(styles['ProfileBlock__Img'], 'ProfileBlock__Img')} src={mainImgUrl} />
+              )}
+
+              {!isLoadingImage && !viewMode && (
                 <PhotoCamIcon
-                  width='1rem'
-                  className={classNames(styles['ProfileBlock__CoverPhotoIcon'], 'ProfileBlock__CoverPhotoIcon')}
+                  onClick={!mainImgUrl ? undefined : onMainImgClick}
+                  width='2.1rem'
+                  className={classNames(styles['ProfileBlock__PhotoCamIcon'], 'ProfileBlock__PhotoCamIcon')}
                 />
               )}
             </div>
           )}
+          <div
+            className={classNames(styles['ProfileBlock__Cover'], 'ProfileBlock__Cover', {
+              [styles['ProfileBlock__Cover--uploaded']]: !!backgroundImgUrl
+            })}
+            onClick={backgroundImgUrl ? undefined : onBackgroundImgClick}>
+            {backgroundImgUrl && (
+              <img
+                className={classNames(styles['ProfileBlock__CoverImg'], 'ProfileBlock__CoverImg', {
+                  [styles['ProfileBlock__Image']]:
+                    backgroundImgUrl === 'https://nid.com.ua/wp-content/themes/realestate-7/images/no-image.png' ||
+                    backgroundImgUrl === 'https://i.ibb.co/dMQPWnZ/no-image.png' ||
+                    backgroundImgUrl === 'https://i.ibb.co/c2yqT5q/image.png'
+                })}
+                src={backgroundImgUrl}
+              />
+            )}
+            {!viewMode && (
+              <div
+                className={classNames(styles['ProfileBlock__CoverPhotoIconCell'], 'ProfileBlock__CoverPhotoIconCell')}
+                onClick={!backgroundImgUrl ? undefined : onBackgroundImgClick}>
+                {!isLoadingImage && (
+                  <PhotoCamIcon
+                    width='1rem'
+                    className={classNames(styles['ProfileBlock__CoverPhotoIcon'], 'ProfileBlock__CoverPhotoIcon')}
+                  />
+                )}
+              </div>
+            )}
+          </div>
         </div>
         <div
           className={classNames(styles['ProfileBlock__Info'], 'ProfileBlock__Info', {
