cfffd046de62909a4844b779a7189342b081cfc5
Armen Mesropyan <amesropyan@bangits.com>
2023-03-23 21:13:30 +0400

    Fix

--- src/components/organisms/wallet-card/WalletCard.tsx
+++ src/components/organisms/wallet-card/WalletCard.tsx
@@ -1,6 +1,5 @@
 import { StatusProps, Typography } from '@my-ui/core';
 import classNames from 'classnames';
-import React, { FC, useRef } from 'react';
 import styles from './WalletCard.module.scss';
 
 export interface WalletCardProps extends StatusProps {
@@ -17,34 +16,32 @@ export interface WalletCardProps extends StatusProps {
   };
 }
 
-
 const WalletCard = ({ cards }: { cards: WalletCardProps[] }) => {
-
   return (
     <div className='d-flex gap-2'>
-      {cards && cards.map((card) => {
-        return <div
-          className={classNames(styles.WalletCardWrapper, {
-            [styles[`WalletCardWrapper--${card.col}`]]: card.col
-          })}>
-          <div className={styles.CardHeader}>
-            <div style={{ display: 'flex' }}>
-              <div className={styles.MainContent}>
-                <Typography className={styles.ContentType} variant='p4'>
-                  {card.walletType || card.noDataText}
-                </Typography>
-                <Typography className={styles.Label} variant='p4'>
-                  {card.balance.label}: <span>{card.balance.value || 0}</span>
-                </Typography>
-                <Typography className={styles.Label} variant='p4'>
-                  {card.account.label}: <span>{card.account.id || card.noDataText}</span>
-                </Typography>
+      {cards &&
+        cards.map((card) => {
+          return (
+            <div
+              className={classNames(styles.WalletCardWrapper, {
+                [styles[`WalletCardWrapper--${card.col}`]]: card.col
+              })}>
+              <div className={styles.CardHeader}>
+                <div className={styles.MainContent}>
+                  <Typography className={styles.ContentType} variant='p4'>
+                    {card.walletType || card.noDataText}
+                  </Typography>
+                  <Typography className={styles.Label} variant='p4'>
+                    {card.balance.label}: <span>{card.balance.value || 0}</span>
+                  </Typography>
+                  <Typography className={styles.Label} variant='p4'>
+                    {card.account.label}: <span>{card.account.id || card.noDataText}</span>
+                  </Typography>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
-      })}
-
+          );
+        })}
     </div>
   );
 };
