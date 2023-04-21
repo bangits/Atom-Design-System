d9b894776b3574a52163cdc1420d24edfbf67ba6
Gor Sargsyan <g.sargsyan@bangits.com>
2023-04-21 15:57:48 +0400

    feat: added file uploader variants

--- src/components/organisms/file-uploader/FileUploader.stories.tsx
+++ src/components/organisms/file-uploader/FileUploader.stories.tsx
@@ -1,5 +1,7 @@
 import { withKnobs } from '@storybook/addon-knobs';
 import { FileUploader } from './FileUploader';
+import { useCallback, useState } from 'react';
+import { FileUploaderVariants } from './enums';
 
 export default {
   title: 'components/organisms/FileUploader',
@@ -7,9 +9,40 @@ export default {
   decorators: [withKnobs]
 };
 
-export const Default = () => {
+export const InputFileUploader = () => {
+  const [error, setError] = useState(null);
+  const [file, setFile] = useState<File>();
+  const [src, setSrc] = useState('');
+  const [percent, setPercent] = useState(0);
+
+  const onChange = useCallback((file) => {
+    setError(null);
+    if (file) {
+      setTimeout(() => {
+        setFile(file);
+        setSrc(
+          'https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80'
+        );
+        setPercent(100);
+      }, 1000);
+    }
+  }, []);
+
+  const onRemove = useCallback(() => {
+    setPercent(0);
+    setFile(null);
+  }, []);
+
   return (
     <FileUploader
+      browseText={file?.name || 'Attach'}
+      imageSrc={src}
+      variant={FileUploaderVariants.input}
+      onRemove={onRemove}
+      onChange={onChange}
+      loadingPercent={percent}
+      onError={(e) => setError(e)}
+      errorMessage={error?.type}
       labelProps={{
         optional: true,
         optionalText: '(optional)',
@@ -19,5 +52,45 @@ export const Default = () => {
         tooltipText: 'How to upload file?'
       }}
     />
+  );
+};
+
+export const LinkFileUploader = () => {
+  const [error, setError] = useState(null);
+  const [file, setFile] = useState<File>();
+  const [src, setSrc] = useState('');
+  const [percent, setPercent] = useState(0);
+
+  const onChange = useCallback((file) => {
+    setError(null);
+    if (file) {
+      setTimeout(() => {
+        setFile(file);
+        setSrc(
+          'https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80'
+        );
+        setPercent(100);
+      }, 1000);
+    }
+  }, []);
+
+  const onRemove = useCallback(() => {
+    setPercent(0);
+    setFile(null);
+  }, []);
+
+  return (
+    <div style={{ marginLeft: '5em', marginTop: '3em' }}>
+      <FileUploader
+        browseText={file?.name || 'Attach'}
+        imageSrc={src}
+        variant={FileUploaderVariants.link}
+        onRemove={onRemove}
+        onChange={onChange}
+        loadingPercent={percent}
+        onError={(e) => setError(e)}
+        errorMessage={error?.type}
+      />
+    </div>
   );
 };
