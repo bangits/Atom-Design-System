fcb277a456c5cd43103fb4fa2a398f8eb1a19a5f
Gor Sargsyan <g.sargsyan@bangits.com>
2023-04-28 12:56:17 +0400

    fix: fixed scss shared logic insertion

--- vite.config.js
+++ vite.config.js
@@ -8,22 +8,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        //🚀🚀🚀🚀🚀 THIS ADDITIONAL SCSS IS BOUND WITH RESOURCES.SCSS 🚀🚀🚀🚀🚀
-        additionalData: [
-        "@import './src/sass/abstracts/variables';",
-        "@import './src/sass/abstracts/mixins';",
-        "@import './src/sass/abstracts/functions';",
-        // Base
-        "@import './src/sass/base/variables';",
-        "@import './src/sass/base/functions';",
-        "@import './src/sass/base/mixins';",
-        // Components
-        "@import './src/sass/components/atoms/main';",
-        "@import './src/sass/components/molecules/main';",
-        "@import './src/sass/components/organisms/main';",
-        "@import './src/sass/components/pages/main';",
-        "@import './src/sass/components/templates/main';",
-        ].join('\n')
+        additionalData: "@import './src/sass/resources.scss';"
       }
     }
   },
