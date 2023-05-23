6db6caa5b42bf7e5760c5787cf86d04a0b504d8f
Gor Sargsyan <g.sargsyan@bangits.com>
2023-05-23 18:22:12 +0400

    feat: added component to place additional fields at sidebar

---
+++ src/components/organisms/sidebar-addittional-field/SidebarAddittionalField.tsx
@@ -0,0 +1,26 @@
+import { ReactNode } from 'react';
+import { Card } from '@my-ui/core';
+import classNames from 'classnames';
+import styles from './SidebarAdditionalField.module.scss';
+
+export interface SidebarAddittionalFieldsProps {
+  label: string;
+  field: string | ReactNode;
+  fieldVariant?: 'text' | 'chip';
+  noDataText?: ReactNode;
+  addSpacings?: boolean;
+}
+
+export const SidebarAddittionalField = ({ label, noDataText, addSpacings, field, fieldVariant = 'text' }) => {
+  return (
+    <Card
+      borderRadius={1.6}
+      className={classNames(styles.StatusView, {
+        [styles['StatusView--with-spacings']]: addSpacings
+      })}>
+      <span>{label || noDataText}</span>
+      <div className={styles['StatusView--container']}></div>
+      <div className={styles[`StatusView--${fieldVariant}`]}>{field}</div>
+    </Card>
+  );
+};
