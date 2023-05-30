13ef18e39465e6ad0c1b8d751bfe00c01d963164
Armen Mesropyan <amesropyan@bangits.com>
2023-05-30 22:31:38 +0400

    feat: add items list and items list top components

---
+++ src/components/templates/items-list/ItemsList.tsx
@@ -0,0 +1,17 @@
+import { ScrollableView, ScrollableViewProps, typedMemo } from '@/atom-design-system';
+import { FC } from 'react';
+import styles from './ItemsList.module.scss';
+
+export interface ItemsListProps {
+  scrollableViewProps: ScrollableViewProps;
+}
+
+const ItemsList: FC<ItemsListProps> = ({ children, scrollableViewProps }) => {
+  return (
+    <ScrollableView height='50vh' {...scrollableViewProps}>
+      <div className={styles.ItemsList}>{children}</div>
+    </ScrollableView>
+  );
+};
+
+export default typedMemo(ItemsList);
