9abbc9362d3595c5fef4a6bb80f496ba5b66f205
Gor Sargsyan <g.sargsyan@bangits.com>
2023-07-19 16:44:44 +0400

    fix: fixed build issue

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
   base: './',
