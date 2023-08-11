5b4b48933bf6a694009cadd6509d24e4da7b4149
Gor Sargsyan <g.sargsyan@bangits.com>
2023-08-11 11:36:28 +0400

    feat: removed provider games component

--- src/components/pages/index.ts
+++ src/components/pages/index.ts
@@ -3,7 +3,6 @@ export * from './loading-page';
 export * from './partner-details';
 export * from './player-details';
 export * from './project-details';
-export * from './provider-details';
 export * from './sign-in';
 export * from './translation-group';
 export * from './user-details';
